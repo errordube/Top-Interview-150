cb1a94a3b31c163fb78dc29e7fbb0de47b0ce607
Aditya Dube <32643633+errordube@users.noreply.github.com>
2026-08-06 21:50:12 -0400

    Implement maxProfit method for stock prices

---
+++ Best Time to Buy and Sell Stock.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
+        best = 0
+        for p in prices:
+            if p < min_price:
+                min_price = p
+            elif p - min_price > best:
+                best = p - min_price
+        return best
+        
