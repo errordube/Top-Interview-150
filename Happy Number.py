9cc46b1b44d291009068f757f3d2735a0b8f3a52
Aditya Dube <32643633+errordube@users.noreply.github.com>
2026-09-14 16:09:20 -0400

    Implement Happy Number algorithm in Python

---
+++ Happy Number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def next_num(x):
+            total = 0
+            while x > 0:
+                x, d = divmod(x, 10)
+                total += d * d
+            return total
+
+        slow, fast = n, next_num(n)
+        while fast != 1 and slow != fast:
+            slow = next_num(slow)
+            fast = next_num(next_num(fast))
+        return fast == 1
+        
