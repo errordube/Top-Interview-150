f539847d4668f6ab07dac59557a4c56f4c94afe1
Aditya Dube <32643633+errordube@users.noreply.github.com>
2026-09-21 17:56:13 -0400

    Implement merge intervals function in Python

---
+++ Merge Intervals.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def merge(self, intervals: list[list[int]]) -> list[list[int]]:
+        intervals.sort(key=lambda x: x[0])
+        merged = []
+        for start, end in intervals:
+            if merged and start <= merged[-1][1]:
+                merged[-1][1] = max(merged[-1][1], end)
+            else:
+                merged.append([start, end])
+        return merged
